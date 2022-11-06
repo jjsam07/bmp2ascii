cd9310bd60cdc5096991b83a42138a12c347313c
JJS4M <sam_jj72@yahoo.com>
2022-11-07 01:32:57 +0800

    added some specific use case function

--- bmp2ascii.py
+++ bmp2ascii.py
@@ -48,20 +48,43 @@ def isprint(c):
 	else:
 		return random.randrange(0x20, 0x7E)
 
+#      0 - 31: ' '
+#     32 - 63: '.'
+#     64 - 95: '!'
+#    96 - 127: '-'
+#   128 - 159: '+'
+#   160 - 191: 'V'
+#   192 - 223: 'J'
+#   224 - 256: 'M'
+
+
 def pxAvg(f, addr, n_bytes):
-	chars = ' .!-+VJM'
-	len_chars = len(chars) - 1
+	chars = b' .!-+VJMM'
 	f.seek(addr)
 	px1 = struct.unpack('B', f.read(1))[0]
 	px2 = 0
 	px3 = 0
 	if n_bytes > 1: px2 = struct.unpack('B', f.read(1))[0]
 	if n_bytes > 2: px3 = struct.unpack('B', f.read(1))[0]
-	avg = int((((px1 + px2 + px3)/n_bytes)/255) * len_chars)
-	if chars[avg] in '\'\"\\':
-		return bytes((('\\'+chars[avg])*2)+' ', 'utf-8')
-	else:
-		return bytes((chars[avg]*2)+' ', 'utf-8')
+	avg = int((((px1 + px2 + px3)/n_bytes)/255) * 8)
+	return chars[avg:avg+1]
+
+def px_color_to_greyscale(f, addr):
+	chars = b' .!-+VJMM'
+	f.seek(addr)
+	pxB = struct.unpack('B', f.read(1))[0]
+	pxG = struct.unpack('B', f.read(1))[0]
+	pxR = struct.unpack('B', f.read(1))[0]
+	index = int((0.2989*pxR + 0.5870*pxG + 0.1140*pxB)/32)
+	return chars[index:index+1]
+	
+def px_color_to_3bit_greyscale(f, addr):
+	f.seek(addr)
+	pxB = struct.unpack('B', f.read(1))[0]
+	pxG = struct.unpack('B', f.read(1))[0]
+	pxR = struct.unpack('B', f.read(1))[0]
+	px = int((0.2989*pxR + 0.5870*pxG + 0.1140*pxB)/32)
+	return px
 	
 def getData(f, addr):
 	f.seek(addr)
@@ -83,7 +106,7 @@ def colorDepth(f):
 	f.seek(0x1C)
 	return struct.unpack('<H', f.read(2))[0]
 
-def bmp2ascii_monochrome(fin, fout):
+def bmp2ascii_monochrome(fin, verbose=False):
 	if (imgWidth(fin) % 32) > 0:
 		width = int((imgWidth(fin) + (32 - (imgWidth(fin) % 32)))/8)
 	else:
@@ -95,27 +118,37 @@ def bmp2ascii_monochrome(fin, fout):
 	pixelData = 0
 	boundary = range(0, width, 4)[-1:][0]
 	bitrange = 32
+	output_buffer = b''
+	output_buffer_lines = [] # separated as lines
 	
-	print(f'Array start = {arrStart}')
-	print(f'ByteWidth = {width}')
-	print(f'Width = {imgWidth(fin)}')
-	print(f'Height = {height}')
-	print(f'Boundary = {boundary}')
+	if verbose:
+		print(f'Array start = {arrStart}')
+		print(f'ByteWidth = {width}')
+		print(f'Width = {imgWidth(fin)}')
+		print(f'Height = {height}')
+		print(f'Boundary = {boundary}')
 	
+	list_index = 0
 	for i in range(height-1, -1, -1):
+		output_buffer_lines.append(b'')
 		for j in range(0, width, 4):
 			if j == boundary:
 				bitrange = imgWidth(fin) % 32
 			pixelData = getData(fin, arrStart+j+(width*i))
 			for k in range(0, bitrange):
 				if (pixelData & (0x80000000 >> k)) != 0:
-					fout.write(b'  ')
+					output_buffer += b'  '
+					output_buffer_lines[list_index] += b'  '
 				else:
-					fout.write(b'##')
-		fout.write(b'\n')
+					output_buffer += b'##'
+					output_buffer_lines[list_index] += b'##'
+		output_buffer += b'\n'
+		list_index += 1
 		bitrange = 32
 	
-def bmp2ascii_greyscale(fin, fout):
+	return output_buffer, output_buffer_lines
+
+def bmp2ascii_greyscale(fin, verbose=False):
 	arrStart = pxArrStart(fin)
 	color_depth = colorDepth(fin)
 	n_bytes = int(color_depth/8)
@@ -124,24 +157,91 @@ def bmp2ascii_greyscale(fin, fout):
 	height = imgHeight(fin)
 	padding = [0, 3, 2, 1][(bwidth % 4)]
 	px = 0
+	output_buffer = b''
+	output_buffer_lines = [] # separated as lines
 	
-	print(f'Pixel array start = {arrStart}')
-	print(f'Width (bytes) = {bwidth}')
-	print(f'Width (px) = {iwidth}')
-	print(f'Height (px) = {height}')
-	print(f'Color depth = {colorDepth(fin)} bit')
-	print(f'Padding = {padding} byte')
+	if verbose:
+		print(f'Pixel array start = {arrStart}')
+		print(f'Width (bytes) = {bwidth}')
+		print(f'Width (px) = {iwidth}')
+		print(f'Height (px) = {height}')
+		print(f'Color depth = {colorDepth(fin)} bit')
+		print(f'Padding = {padding} byte')
 	
-	fout.write(b'var caloocan_boy = [\'') #
-	
+	list_index = 0
 	for i in range(height-1, -1, -1):
+		output_buffer_lines.append(b'')
 		for j in range(0, iwidth):
 			px = pxAvg(fin, arrStart+(j*n_bytes)+((bwidth+padding)*i), n_bytes)
-			fout.write(px)
-		if i == 0:
-			fout.write(b'\'];\n')
-		else:
-			fout.write(b'\',\n\'')
+			output_buffer += px
+			output_buffer_lines[list_index] += px
+		output_buffer += b'\n'
+		list_index += 1
+	
+	return output_buffer, output_buffer_lines
+
+def bmp2ascii_color(fin, verbose=False, px_array_column_start_offset=0, px_array_column_end_offset=0, px_array_row_start_offset=0, px_array_row_end_offset=0):
+	arrStart = pxArrStart(fin)
+	color_depth = colorDepth(fin)
+	n_bytes = int(color_depth/8)
+	iwidth = imgWidth(fin)
+	bwidth = n_bytes*iwidth
+	height = imgHeight(fin)
+	padding = [0, 3, 2, 1][(bwidth % 4)]
+	px = 0
+	output_buffer = b''
+	output_buffer_lines = [] # separated as lines
+	
+	if verbose:
+		print(f'Pixel array start = {arrStart}')
+		print(f'Width (bytes) = {bwidth}')
+		print(f'Width (px) = {iwidth}')
+		print(f'Height (px) = {height}')
+		print(f'Color depth = {colorDepth(fin)} bit')
+		print(f'Padding = {padding} byte')
+	
+	list_index = 0
+	for i in range(height-1-px_array_row_start_offset, -1+px_array_row_end_offset, -1):
+		output_buffer_lines.append(b'')
+		for j in range(0+px_array_column_start_offset, iwidth-px_array_column_end_offset):
+			px = px_color_to_greyscale(fin, arrStart+(j*n_bytes)+((bwidth+padding)*i))
+			output_buffer += px
+			output_buffer_lines[list_index] += px
+		output_buffer += b'\n'
+		list_index += 1
+	
+	return output_buffer, output_buffer_lines
+	
+def color_to_3bit_greyscale(fin, verbose=False, px_array_column_start_offset=0, px_array_column_end_offset=0, px_array_row_start_offset=0, px_array_row_end_offset=0):
+	arrStart = pxArrStart(fin)
+	color_depth = colorDepth(fin)
+	n_bytes = int(color_depth/8)
+	iwidth = imgWidth(fin)
+	bwidth = n_bytes*iwidth
+	height = imgHeight(fin)
+	padding = [0, 3, 2, 1][(bwidth % 4)]
+	px = 0
+	output_buffer = []
+	output_buffer_lines = [] # separated as lines
+	
+	if verbose:
+		print(f'Pixel array start = {arrStart}')
+		print(f'Width (bytes) = {bwidth}')
+		print(f'Width (px) = {iwidth}')
+		print(f'Height (px) = {height}')
+		print(f'Color depth = {colorDepth(fin)} bit')
+		print(f'Padding = {padding} byte')
+	
+	list_index = 0
+	for i in range(height-1-px_array_row_start_offset, -1+px_array_row_end_offset, -1):
+		output_buffer_lines.append([])
+		for j in range(0+px_array_column_start_offset, iwidth-px_array_column_end_offset):
+			px = px_color_to_3bit_greyscale(fin, arrStart+(j*n_bytes)+((bwidth+padding)*i))
+			output_buffer.append(px)
+			output_buffer_lines[list_index].append(px)
+		list_index += 1
+	
+	return output_buffer, output_buffer_lines
 	
 def main():
 	count = 1
@@ -163,14 +263,20 @@ def main():
 			mode = 'g'
 			count += 1
 			continue
+		if (i == '-c') and (mode == 0):
+			mode = 'c'
+			count += 1
+			continue
 		count += 1
 		
 	if (mode == 'm'):
-		bmp2ascii_monochrome(fin, fout)
+		fout.write(bmp2ascii_monochrome(fin)[0])
 	elif (mode == 'g'):
-		bmp2ascii_greyscale(fin, fout)
+		fout.write(bmp2ascii_greyscale(fin)[0])
+	elif (mode == 'c'):
+		fout.write(bmp2ascii_color(fin)[0])
 	else:
-		print("Specify a mode bro! (-m | -g)\n")
+		print("Specify a mode bro! (-m | -g | -c)\n")
 	
 	fin.close()
 	fout.close()
